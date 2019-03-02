56d6affbd5b5ee0420e1e15d6664b775d750dc0d
Nick Benoit <nickbenoit@Nicks-MacBook-Pro.local>
2019-03-02 10:40:57 -0500

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+let Navbar = () => {
+    return <div>
+        <ul className="navbar">
+            <li>Home</li>
+            <li>Ingredients</li>
+            <li>About</li>
+        </ul>
+    </div>
+}
+
+
+export default Navbar;
