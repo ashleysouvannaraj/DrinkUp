f02dccdf571072b4c354a6e0a5ca54f8ad50deda
Nick Benoit <nickbenoit@Nicks-MacBook-Pro.local>
2019-03-05 17:54:26 -0500

    have the header with the links rendering the different link components:

---
+++ src/Actions/actionCreators.js
@@ -0,0 +1,9 @@
+const mapDispatchToProps = (dispatch) => ({
+    startGame: () => dispatch({type:'START_GAME'}),
+    selectLiquor: () => dispatch({type: 'SELECT_LIQUOR'}),
+    selectLiquer: () => dispatch({type: 'SELECT_LIQUER'}),
+    selectMixers: () => dispatch({type: 'SELECT_MIXERS'}),
+    selectGarnishes: () => dispatch({type: 'SELECT_GARNISHES'}),
+    submitDrink: () => dispatch({tyle: 'SUBMIT_DRINK'})
+})
+
--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { NavLink, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import { connect } from "react-redux";
+import {withRouter} from 'react-router-dom'
+// import mapDispatchToProps from "./Actions/actionCreators.js";
+import Level1 from "./Levels/Level1.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar/>
       </div>
     );
   }
 }
 
-export default App;
+
+const mapStateToProps = (state) => ({
+  isGameStarted: state.isGameStarted,
+  ingredients: state.ingredients
+
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  startGame: () => dispatch({type:'START_GAME'}),
+  selectLiquor: () => dispatch({type: 'SELECT_LIQUOR'}),
+  selectLiquer: () => dispatch({type: 'SELECT_LIQUER'}),
+  selectMixers: () => dispatch({type: 'SELECT_MIXERS'}),
+  selectGarnishes: () => dispatch({type: 'SELECT_GARNISHES'}),
+  submitDrink: () => dispatch({tyle: 'SUBMIT_DRINK'})
+})
+
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App));
